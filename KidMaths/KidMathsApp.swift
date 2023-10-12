a32d945bb707d03f9cb811ea6d9c8d29a45ae0cc
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-10-12 16:25:58 +0100

    Initial Commit

---
+++ KidMaths/KidMathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidMathsApp.swift
+//  KidMaths
+//
+//  Created by Godwin IE on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct KidMathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
